f3ab83dd78e059f4b8ce10e1d7173740c865b636
Sherri Ase <shirin.asenova90@gmail.com>
2023-11-15 21:25:07 +0200

    add doctor list and file add appointments list and file

--- src/Appointment.java
+++ src/Appointment.java
@@ -1,2 +1,68 @@
+import java.util.Date;
+
 public class Appointment {
+    //appointment_id, patient_id, type_of_examination, date, time, doctor_id
+    private int appointmentId;
+    private int patientId;
+    private String typeOfExamination;
+    private String date;
+    private String time;
+    private int doctorId;
+
+    public Appointment(int appointmentId, int patientId, String typeOfExamination, String date, String time, int doctorId) {
+        this.appointmentId = appointmentId;
+        this.patientId = patientId;
+        this.typeOfExamination = typeOfExamination;
+        this.date = date;
+        this.time = time;
+        this.doctorId = doctorId;
+    }
+
+    public int getAppointmentId() {
+        return appointmentId;
+    }
+
+    public void setAppointmentId(int appointmentId) {
+        this.appointmentId = appointmentId;
+    }
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getTypeOfExamination() {
+        return typeOfExamination;
+    }
+
+    public void setTypeOfExamination(String typeOfExamination) {
+        this.typeOfExamination = typeOfExamination;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public int getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(int doctorId) {
+        this.doctorId = doctorId;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -21,11 +21,54 @@ public class Main {
                 writer.write(patient.getPatientId() + "," + patient.getFirstName() + "," + patient.getLastName() + "," + patient.getAge());
                 writer.newLine();
             }
-
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        Doctor[] doctors = new Doctor[12];
+        doctors[0] = new Doctor(1, "Gergana", "Georgieva", "Anesthesiology");
+        doctors[1] = new Doctor(2, "Georgi", "Georgiev", "Internal diseases");
+        doctors[2] = new Doctor(3, "Ivanka", "Ivanova", "Gastroenterology");
+        doctors[3] = new Doctor(4, "Ivan", "Ivanov", "Endocrinology");
+        doctors[4] = new Doctor(5, "Iordanka", "Iordanova", "Cardiology");
+        doctors[5] = new Doctor(6, "Iordan", "Iordanov", "Dermatology");
+        doctors[6] = new Doctor(7, "Simona", "Simeonova", "Anesthesiology");
+        doctors[7] = new Doctor(8, "Simeon", "Simeonov", "Internal diseases");
+        doctors[8] = new Doctor(9, "Dimitar", "Dimitrov", "Gastroenterology");
+        doctors[9] = new Doctor(10, "Denitza", "Dimitrova", "Endocrinology");
+        doctors[10] = new Doctor(11, "Slavka", "Slovakova", "Cardiology");
+        doctors[11] = new Doctor(12, "Sirma", "Krasimireva", "Dermatology");
+        File doctorList = new File("doctors.csv");
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(doctorList))){
+            writer.write("doctor_id, first_name, last_name, specialty");
+            writer.newLine();
+            for(Doctor doctor : doctors){
+                writer.write(doctor.getDoctorId() + "," + doctor.getFirstName() + "," + doctor.getLastName() + "," + doctor.getSpecialty());
+                writer.newLine();
+            }
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
 
+        List<Appointment> appointments = new ArrayList<>();
+        appointments.add(new Appointment(1,1,"initial","28-06-2021","10:00",2));
+        appointments.add(new Appointment(2,5,"secondary","21-06-2021","12:30",5));
+        appointments.add(new Appointment(3,3,"consultation","15-06-2021","10:30",2));
+        appointments.add(new Appointment(4,4,"initial","15-06-2021","12:30",4));
+        appointments.add(new Appointment(5,2,"procedure","15-06-2021","12:30",7));
+
+        File appointmentsList = new File("appointments.csv");
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(appointmentsList))){
+            writer.write("appointment_id,patient_id,type_of_examination,date,time,doctor_id");
+            writer.newLine();
+            for(int i = 0; i < appointments.size(); i++){
+                writer.write(appointments.get(i).getAppointmentId() + "," + appointments.get(i).getPatientId() + ","
+                        + appointments.get(i).getTypeOfExamination() + "," + appointments.get(i).getDate()+ "," + appointments.get(i).getTime()
+                        + ","+ appointments.get(i).getDoctorId());
+                writer.newLine();
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
 
     }
 }
