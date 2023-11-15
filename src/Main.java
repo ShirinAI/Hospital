7efd72b647d33e9578ac5851e88e73284c3e88d9
Sherri Ase <shirin.asenova90@gmail.com>
2023-11-15 13:16:41 +0200

    adding Patient class, array and file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       Patient[] patients = new Patient[5];
+        patients[0] = new Patient(1, "Sarah", "Jones", 40);
+        patients[1] = new Patient(2, "John", "Snow", 35);
+        patients[2] = new Patient(3, "Tim", "Danes", 77);
+        patients[3] = new Patient(4, "Philip", "Summer", 56);
+        patients[4] = new Patient(5, "Donna", "Matthews", 57);
+        File patientsList =  new File("patients.csv");
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(patientsList))){
+            writer.write("patient_id, first_name, last_name, age");
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+
     }
 }
---
+++ src/Patient.java
@@ -0,0 +1,45 @@
+public class Patient {
+    private int patientId;
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Patient(int patientId, String firstName, String lastName, int age) {
+        this.patientId = patientId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
