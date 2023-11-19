b1bca2e608fe4329db703c47fd160ee24e0c5d55
Sherri Ase <shirin.asenova90@gmail.com>
2023-11-19 15:36:19 +0200

    add first screen and doctor login

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,37 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    public static void doctorLogin(int id, String lastName, Doctor[] doctors){
+        Doctor matchingDoctor = null;
+
+        for (Doctor doctor : doctors) {
+            if (doctor.getDoctorId() == id && doctor.getLastName().equals(lastName)) {
+                matchingDoctor = doctor;
+                break;
+            }
+        }
+        if (matchingDoctor != null) {
+            System.out.println("Login successful. Welcome, Dr. " + matchingDoctor.getFirstName() + " " + matchingDoctor.getLastName());
+        } else {
+            System.out.println("Invalid login credentials for doctor.");
+        }
+    }
     public static void main(String[] args) {
-       Patient[] patients = new Patient[5];
+        Patient[] patients = new Patient[5];
         patients[0] = new Patient(1, "Sarah", "Jones", 40);
         patients[1] = new Patient(2, "John", "Snow", 35);
         patients[2] = new Patient(3, "Tim", "Danes", 77);
         patients[3] = new Patient(4, "Philip", "Summer", 56);
         patients[4] = new Patient(5, "Donna", "Matthews", 57);
-        File patientsList =  new File("patients.csv");
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(patientsList))){
+        File patientsList = new File("patients.csv");
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(patientsList))) {
             writer.write("patient_id, first_name, last_name, age");
             writer.newLine();
-            for(Patient patient: patients){
+            for (Patient patient : patients) {
                 writer.write(patient.getPatientId() + "," + patient.getFirstName() + "," + patient.getLastName() + "," + patient.getAge());
                 writer.newLine();
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
         Doctor[] doctors = new Doctor[12];
@@ -38,39 +53,70 @@ public class Main {
         doctors[10] = new Doctor(11, "Slavka", "Slovakova", "Cardiology");
         doctors[11] = new Doctor(12, "Sirma", "Krasimireva", "Dermatology");
         File doctorList = new File("doctors.csv");
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(doctorList))){
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(doctorList))) {
             writer.write("doctor_id, first_name, last_name, specialty");
             writer.newLine();
-            for(Doctor doctor : doctors){
+            for (Doctor doctor : doctors) {
                 writer.write(doctor.getDoctorId() + "," + doctor.getFirstName() + "," + doctor.getLastName() + "," + doctor.getSpecialty());
                 writer.newLine();
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
 
         List<Appointment> appointments = new ArrayList<>();
-        appointments.add(new Appointment(1,1,"initial","28-06-2021","10:00",2, true));
-        appointments.add(new Appointment(2,5,"secondary","21-06-2021","12:30",5, true));
-        appointments.add(new Appointment(3,3,"consultation","15-06-2021","10:30",2, true));
-        appointments.add(new Appointment(4,4,"initial","15-06-2021","12:30",4, true));
-        appointments.add(new Appointment(5,2,"procedure","15-06-2021","12:30",7, true));
+        appointments.add(new Appointment(1, 1, "initial", "28-06-2021", "10:00", 2, true));
+        appointments.add(new Appointment(2, 5, "secondary", "21-06-2021", "12:30", 5, true));
+        appointments.add(new Appointment(3, 3, "consultation", "15-06-2021", "10:30", 2, true));
+        appointments.add(new Appointment(4, 4, "initial", "15-06-2021", "12:30", 4, true));
+        appointments.add(new Appointment(5, 2, "procedure", "15-06-2021", "12:30", 7, true));
 
         File appointmentsList = new File("appointments.csv");
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(appointmentsList))){
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(appointmentsList))) {
             writer.write("appointment_id,patient_id,type_of_examination,date,time,doctor_id");
             writer.newLine();
-            for(int i = 0; i < appointments.size(); i++){
+            for (int i = 0; i < appointments.size(); i++) {
                 writer.write(appointments.get(i).getAppointmentId() + "," + appointments.get(i).getPatientId() + ","
-                        + appointments.get(i).getTypeOfExamination() + "," + appointments.get(i).getDate()+ "," + appointments.get(i).getTime()
-                        + ","+ appointments.get(i).getDoctorId());
+                        + appointments.get(i).getTypeOfExamination() + "," + appointments.get(i).getDate() + "," + appointments.get(i).getTime()
+                        + "," + appointments.get(i).getDoctorId());
                 writer.newLine();
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
 
+        //VHOD
+        Scanner scanner = new Scanner(System.in);
+        int option;
+        do {
+            System.out.println("Select an option:");
+            System.out.println("1. Login as doctor");
+            System.out.println("2. Login as patient");
 
+            option = scanner.nextInt();
+            scanner.nextLine();
 
+            switch (option) {
+                case 1:
+                    System.out.println("Please enter your doctor's ID: ");
+                    int id = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.println("Please enter your last name: ");
+                    String surname = scanner.nextLine();
+                    doctorLogin(id, surname, doctors);
+                    System.out.println("I am a doctor");
+                    break;
+                case 2:
+                    //patientLogin();
+                    System.out.println("I am a patient");
+                    break;
+
+                default:
+                    System.out.println("Invalid option. Please choose from the options below.");
+                    break;
+            }
+        } while (option != 0);
     }
+
+
 }
